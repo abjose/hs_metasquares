3199b4a02ac101390c1de78c0bac3b121799b37e
Alex Jose <ajose@caltech.edu>
2014-06-09 19:08:36 -0700

    add distance measure to point

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
-
 class Point(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
     def __repr__(self):
         return "Point(" + str(self.x) + ", " + str(self.y) + ")"
+    def __ew__(self, other):
+        return self.x == other.x and self.y == other.y
+    def dist(self, other):
+        # NOTE: NOT ACTUAL EUCLIDEAN DISTANCE
+        # return the squared distance (to avoid sqrt operation) 
+        return (self.x-other.x)**2 + (self.y-other.y)**2
