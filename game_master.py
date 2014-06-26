6ae7711b8ea916c352b084ea59ca25f4f022b23a
Alex Jose <ajose@caltech.edu>
2014-06-26 02:23:34 -0400

    change arg order for add_player

--- game_master.py
+++ game_master.py
@@ -23,11 +23,11 @@ class GameMaster(object):
         self.games[str(game_name)] = Game(r, c, num_players, score_limit)
         return True, "Game '" + str(game_name) + "' successfully created."
 
-    def add_player(self, game_name, player_name):
+    def add_player(self, player_name, game_name):
         if game_name not in self.games:
             return False,  "Player not added - that game doesn't exist."
-        if player_name in [player for game in self.games.values() for player in game.players]:
-            return False, "Player not added - that name already exists on the server."
+        #if player_name in [player for game in self.games.values() for player in game.players]:
+        #    return False, "Player not added - that name already exists on the server."
         return self.games[game_name].add_player(player_name)
             
     def move(self, game_name, r, c, player_name):
@@ -47,11 +47,11 @@ if __name__=='__main__':
     print GM.add_game('test1', 10, 10, 2, 150)
     print GM.add_game('test2', 10, 10, 2, 150)
     print GM.add_game('test3', 10, 10, 2, 150)
-    print GM.add_player('test1', 'x')
-    print GM.add_player('test1', 'x')
-    print GM.add_player('test1', ' ')
-    print GM.add_player('test1', 'y')
-    print GM.add_player('test1', 'z')
+    print GM.add_player('x', 'test1')
+    print GM.add_player('x', 'test1')
+    print GM.add_player(' ', 'test1')
+    print GM.add_player('y', 'test1')
+    print GM.add_player('z', 'test1')
     print GM.move('test1', 0, 0, 'x')
     print GM.move('test1', 0, 1, 'y')
     print GM.move('test1', 0, 0, 'z')
